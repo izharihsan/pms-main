9730a4b075ae3fc7acc24237178fd0eed22f801c
inu847 <adin72978@gmail.com>
2024-05-06 10:36:06 +0700

    update templaye

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>@yield('title') | PMS</title>
+
+	<!-- Global stylesheets -->
+	<link href="{{ asset('assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/icons/phosphor/styles.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="{{ asset('assets/demo/demo_configurator.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3.min.js') }}"></script>
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3_tooltip.js') }}"></script>
+
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script src="{{ asset('assets/demo/pages/dashboard.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/streamgraph.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/sparklines.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/lines.js') }}"></script>	
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/areas.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/donuts.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bars.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/progress.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/heatmaps.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/pies.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bullets.js') }}"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		@include('include.sidebar_admin')  
+
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			@include('include.navbar_admin')
+
+
+			<!-- Inner content -->
+			<div class="content-inner">
+                @yield('content')
+			</div>
+			<!-- /inner content -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+    @include('include.rightbar_admin')
+</body>
+</html>
