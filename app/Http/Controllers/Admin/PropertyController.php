be67b5865f8046f1d2aa276382c6d4391a27a643
Adi Nugroho <adin72978@gmail.com>
2024-05-20 17:43:05 +0700

    open api resource facilities property

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -35,7 +35,7 @@ class PropertyController extends Controller
     {
         $apiResource = new ApiResourcesController();
         // $category_facilities = $apiResource->categoryFacilities();
-        // $facilities = $apiResource->propertyFacilities()['data']['categories'];
+        $facilities = $apiResource->propertyFacilities()['data']['categories'];
         // $room_type = $apiResource->roomType();
         // $bed_type = $apiResource->bedType();
 
@@ -99,7 +99,7 @@ class PropertyController extends Controller
             ['id' => 15, 'name' => 'Style 15'],
         ];
 
-        $facilities = [];
+        // $facilities = [];
 
         if ($request->has('step')) {
             return view('admin.property.step' . $request->step, compact('category_facilities', 'facilities', 'city', 'district', 'village', 'property_style'));
@@ -113,7 +113,6 @@ class PropertyController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
         try {
             return $this->atomic(function () use ($request) {
                 $property=Property::create([
